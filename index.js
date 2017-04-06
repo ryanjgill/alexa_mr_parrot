8b356865ebd411d21d24a164724ad7fa368a790f
Ryan Gill <ryanjgill2@gmail.com>
2017-04-05 19:06:18 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,93 @@
+'use strict';
+
+const Alexa = require('alexa-sdk')
+	, request = require('request')
+	, APP_ID = 'amzn1.ask.skill.93334129-07bd-478c-bec0-6a32768d0a89'
+	, SKILL_NAME = 'Sounder'
+	;
+
+function getSoundFromSoundy(term, callback) {
+  let searchUrl = `https://www.soundy.top/api/sounds?${term}`;
+
+  return request(searchUrl, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      // parse the body and format for desired output
+      let data = JSON.parse(body)[0]; // taking the first item in the returned results
+      return callback(data.name, data.url, searchUrl);
+    } else {
+      throw(new Error('Request failed!'));
+    }
+  });
+}
+
+exports.handler = function(event, context, callback) {
+  let alexa = Alexa.handler(event, context);
+  alexa.APP_ID = APP_ID;
+  alexa.registerHandlers(handlers);
+  alexa.execute();
+};
+
+let handlers = {
+  'LaunchRequest': function() {
+    this.emit('SearchForSound');
+  },
+  'SearchForSoundIntent': function() {
+    this.emit('SearchForSound');
+  },
+  'SearchForSound': function() {
+    let term =  this.event.request && this.event.request.intent && this.event.request.intent.slots && this.event.request.intent.slots.Term
+      ? this.event.request.intent.slots.Term.value
+      : '';
+
+
+    getSoundFromSoundy(term, function(name, url, searchUrl) {
+      let audioUrl = url.split('.mp3')[0] + '.mp3';
+      let audioHtml = `<audio src="http:${audioUrl}" />`
+      let speechOutput = `I searched for term: ${term} and I found a sound matching the name: ${name}. ${audioHtml}`;
+      let cardTitle = `Sound found!`;
+      let cardContent = url;
+
+      //this.emit(':tellWithCard', speechOutput, cardTitle, cardContent);
+
+      let response = {
+        version: "1.0",
+        response: {
+            shouldEndSession: true,
+            directives: [
+                {
+                    type: "AudioPlayer.Play",
+                    playBehavior: "REPLACE_ALL",
+                    audioItem: {
+                        stream: {
+                            url: `https:${audioUrl}`,
+                            token: "0",
+                            expectedPreviousToken: null,
+                            offsetInMilliseconds: 200
+                        }
+                    }
+                }
+            ]
+        }
+    }
+
+    this.context.succeed(response);
+
+    }.bind(this));
+  },
+  'AMAZON.HelpIntent': function() {
+    let reprompt = `What can I help you with?`
+     , speechOutput = `You can ask me for a sound, or, you can say exit... ${reprompt}`
+     ;
+
+    this.emit(':ask', speechOutput, reprompt);
+  },
+  'AMAZON.CancelIntent': function() {
+    this.emit(':tell', 'Goodbye!');
+  },
+  'AMAZON.StopIntent': function() {
+    this.emit(':tell', 'Goodbye!');
+  }, 
+  'Unhandled': function() {
+    this.emit(':ask', 'Sorry, something went wrong.');
+  }
+};
---
+++ readme.md
@@ -0,0 +1,17 @@
+## Alexa Sounder
+Plays audio clips from soundy.
+
+#### Engage Alexa like:
+`Alexa ask Sounder for Ain't nobody got time for that.`
+
+`Alexa search Sounder for Dogs.`
+
+`Alexa ask Sounder to find Mario`
+
+`Echo Sounder find Laughing`
+
+###### Lamda functions 
+https://console.aws.amazon.com/lambda/home?region=us-east-1#/functions?display=list
+
+###### Alexa skills
+https://developer.amazon.com/edw/home.html#/skills/list

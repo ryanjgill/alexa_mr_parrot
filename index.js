9761a72b8af577aeccd8843d6bb1ceeda392aee4
Ryan Gill <ryanjgill2@gmail.com>
2017-04-10 22:53:41 -0500

    selectResult, multiple sounds response

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
 'use strict';
 
 const Alexa = require('alexa-sdk')
-	, request = require('request')
-	, APP_ID = 'amzn1.ask.skill.93334129-07bd-478c-bec0-6a32768d0a89'
-	, SKILL_NAME = 'Sounder'
-	;
+  , request = require('request')
+  , APP_ID = 'amzn1.ask.skill.93334129-07bd-478c-bec0-6a32768d0a89'
+  , SKILL_NAME = 'Parrot'
+  ;
 
-function getSoundFromSoundy(term, callback) {
+function getSoundFromSoundy(term, cb) {
   const baseUrl = `https://www.soundy.top/api/sounds`;
   let searchUrl = term ? `${baseUrl}?q=${term}` : baseUrl;
 
   console.log('URL: ', searchUrl);
 
-  return request(searchUrl, function(error, response, body) {
+  return request(searchUrl, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      // parse the body and format for desired output
-      let data = JSON.parse(body)[0]; // taking the first item in the returned results
-
-      if (!data || !data.hasOwnProperty('url')) {
-        return callback({
-          type: 'no_results',
-          term: term
-        });
-      }
-
-      return callback(null, data.name, data.url, searchUrl);
+      return cb(null, JSON.parse(body));
     } else {
       console.log('ERROR: ', error);
-      return callback({
+      return cb({
         type: 'service_down',
         term: term
       });
@@ -35,7 +25,7 @@ function getSoundFromSoundy(term, callback) {
   });
 }
 
-exports.handler = function(event, context, callback) {
+exports.handler = function (event, context, callback) {
   let alexa = Alexa.handler(event, context);
   alexa.APP_ID = APP_ID;
   alexa.registerHandlers(handlers);
@@ -43,26 +33,21 @@ exports.handler = function(event, context, callback) {
 };
 
 let handlers = {
-  'LaunchRequest': function() {
+  'LaunchRequest': function () {
     this.emit('SearchForSound');
   },
-  'SearchForSoundIntent': function() {
+  'SearchForSoundIntent': function () {
     this.emit('SearchForSound');
   },
-  'SearchForSound': function() {
-    let term =  this.event.request && this.event.request.intent && this.event.request.intent.slots && this.event.request.intent.slots.Term
+  'SearchForSound': function () {
+    let term = this.event.request &&
+      this.event.request.intent &&
+      this.event.request.intent.slots &&
+      this.event.request.intent.slots.Term
       ? this.event.request.intent.slots.Term.value
       : '';
 
-
-    getSoundFromSoundy(term, function(err, name, url, searchUrl) {
-      // if no matching results, respond with search term.
-      if (err && err.type === 'no_results') {
-        let speechOutput = `I searched for term: ${err.term} but didn't find any matches.`;
-        this.emit(':tellWithCard', speechOutput, 'No matching results', speechOutput);
-        return;
-      }
-
+    getSoundFromSoundy(term, function (err, results) {
       // if api error, respond with message saying its down
       if (err && err.type === 'service_down') {
         let speechOutput = `The sound a. p. i. service appears to be down. Please try again later.`;
@@ -70,49 +55,90 @@ let handlers = {
         return;
       }
 
-      // Play audio from url of matching clip
-      let audioUrl = url.split('.mp3')[0] + '.mp3';
-      let audioHtml = `<audio src='http:${audioUrl}' />`
-      let response = {
-        version: '1.0',
-        response: {
-            shouldEndSession: true,
-            directives: [
-                {
-                    type: 'AudioPlayer.Play',
-                    playBehavior: 'REPLACE_ALL',
-                    audioItem: {
-                        stream: {
-                            url: `https:${audioUrl}`,
-                            token: '0',
-                            expectedPreviousToken: null,
-                            offsetInMilliseconds: 0
-                        }
-                    }
-                }
-            ]
-        }
-    }
+      // if no matching results, respond with search term.
+      if (results && results.length === 0) {
+        let speechOutput = `I searched for term: ${term} but didn't find any matches.`;
+        this.emit(':tellWithCard', speechOutput, 'No matching results', speechOutput);
+        return;
+      }
 
-    this.context.succeed(response);
+      if (results && results.length > 1) {
+        this.emit('MultipleSearchResults', results);
+        return;
+      }
 
+      // Play audio if only 1 result
+      this.emit('PlayAudio', results[0]);
     }.bind(this));
   },
-  'AMAZON.HelpIntent': function() {
+  'AMAZON.HelpIntent': function () {
     let reprompt = `What can I help you with?`
-     , speechOutput = `You can ask me for a sound, or, you can say exit... ${reprompt}`
-     ;
+      , speechOutput = `You can ask me for a sound, or, you can say exit... ${reprompt}`
+      ;
 
     this.emit(':ask', speechOutput, reprompt);
   },
 
-  'AMAZON.CancelIntent': function() {
+  'AMAZON.CancelIntent': function () {
     this.emit(':tell', 'Goodbye!');
   },
-  'AMAZON.StopIntent': function() {
+  'AMAZON.StopIntent': function () {
     this.emit(':tell', 'Goodbye!');
-  }, 
-  'Unhandled': function() {
-    this.emit(':ask', 'Sorry, something went wrong.');
+  },
+  'MultipleSearchResults': function (term, results) {
+    let speechOutput = `I found a lot of answers, pick a number between 1 and ${results.length}.`
+    let reprompt = `Pick a number between 1 and ${results.length}.`;
+
+    this.attributes['searchResults'] = results;
+
+    this.emit(':ask', speechOutput, reprompt);
+  },
+  'OutsideRange': function (index) {
+    let totalResults = this.attributes.searchResults.length;
+    let reprompt = 'pick a number between 1 and ${this.attributes.searchResults.length}';
+    let speechOutput = `${index} is outside the range, Please ${reprompt}`;
+    this.emit(':ask', speechOutput, reprompt);
+  },
+  'SelectResult': function () {
+    let index = this.event.request &&
+      this.event.request.intent &&
+      this.event.request.intent.slots &&
+      this.event.request.intent.slots.ResultIndex
+      ? this.event.request.intent.slots.ResultIndex.value - 1
+      : 0;
+
+    if (index > this.attributes.searchResults.length - 1) {
+      this.emit('OutsideRange', index);
+      return;
+    }
+
+    this.emit('PlayAudio', this.attributes.searchResults[index]);
+  },
+  'PlayAudio': function (audio) {
+    // Play audio from url of matching clip
+    let audioUrl = audio.url.split('.mp3')[0] + '.mp3';
+    let audioHtml = `<audio src='http:${audioUrl}' />`
+    let response = {
+      version: '1.0',
+      response: {
+        shouldEndSession: true,
+        directives: [
+          {
+            type: 'AudioPlayer.Play',
+            playBehavior: 'REPLACE_ALL',
+            audioItem: {
+              stream: {
+                url: `https:${audioUrl}`,
+                token: '0',
+                expectedPreviousToken: null,
+                offsetInMilliseconds: 0
+              }
+            }
+          }
+        ]
+      }
+    }
+
+    this.context.succeed(response);
   }
 };
